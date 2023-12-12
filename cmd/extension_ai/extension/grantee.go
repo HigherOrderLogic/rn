af39f6741244ee54fc13a5cd6ed4164cd4875357
ernestrc <ernest@unstable.build>
2023-12-12 11:43:02 -0800

    Move extension_ai's default openai grantee outside of main
    Enables re-use of grantee

--- cmd/extension_ai/extension/grantee.go
+++ cmd/extension_ai/extension/grantee.go
@@ -1,9 +1,16 @@
 package extension
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/ernestrc/blue/logging"
+	log "github.com/sirupsen/logrus"
 	configapi "unstable.build/go-tui/api/config"
 	textapi "unstable.build/go-tui/api/text"
 	"unstable.build/go-tui/cmd/extension_ai/backend"
+	"unstable.build/go-tui/cmd/extension_ai/backend/openai"
 	"unstable.build/go-tui/cmd/extension_ai/dialogue"
 	"unstable.build/go-tui/extension"
 	extutil "unstable.build/go-tui/extension/util"
@@ -29,4 +36,96 @@ func GranteeWithService(
 		AIHandlerCommands, commandEventHandler, AIHandlerEvents,
 		AIHandlerPermissions...)
 	return grantee, perms
+}
+
+const (
+	defaultDefaultModel = openai.GPT3Dot5Turbo
+	defaultBaseURL      = "" // uses openai's default base URL
+)
+
+// DefaultOpenAIGrantee returns GranteeWithService satisfied by an OpenAI-like
+// backend service. Configuration can set a 'base_url' to override openai's
+// service URL with a custom one.
+func DefaultOpenAIGrantee() (extension.Grantee, []extension.Permission) {
+	openaiSvc := func(
+		config configapi.Config, availableModels map[string]int, model string,
+	) (backend.Service, error) {
+		apiKey, err := config.GetString("api_key")
+		if err != nil {
+			err = fmt.Errorf("failed to get 'api_key' from config: %w", err)
+			return nil, err
+		}
+		openaiConfig := openai.Config{
+			Model: model,
+		}
+		openaiConfig.BaseURL, err = config.GetString("base_url")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'base_url' from config: %w", err)
+				return nil, err
+			}
+			openaiConfig.BaseURL = defaultBaseURL
+		}
+		openaiConfig.FrequencyPenalty, err = config.GetFloat("frequency_penalty")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'frequency_penalty' from config: %w", err)
+				return nil, err
+			}
+		}
+		openaiConfig.MaxTokens, err = config.GetInt("max_tokens")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'max_tokens' from config: %w", err)
+				return nil, err
+			}
+		}
+		openaiConfig.PresencePenalty, err = config.GetFloat("presence_penalty")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'presence_penalty' from config: %w", err)
+				return nil, err
+			}
+		}
+		openaiConfig.Temperature, err = config.GetFloat("temperature")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'temperature' from config: %w", err)
+				return nil, err
+			}
+		}
+		openaiConfig.TopP, err = config.GetFloat("top_p")
+		if err != nil {
+			if !errors.Is(err, configapi.ErrNotFound) {
+				err = fmt.Errorf("failed to get 'top_p' from config: %w", err)
+				return nil, err
+			}
+		}
+		return openai.NewClient(apiKey, openaiConfig, availableModels), nil
+	}
+	return GranteeWithService(openaiSvc,
+		openai.AvailableModels(), defaultDefaultModel,
+		dialogue.WithCompleter(dialogue.FuncCompleter(logCompletion)),
+		dialogue.WithInitialContext([]backend.ChatCompletionMessage{
+			{
+				Role: backend.RoleSystem,
+				Content: "You are a helpful coding assistant. Any code changes must be " +
+					" answered in diff format.",
+			},
+		}),
+	)
+}
+
+func logCompletion(
+	ctx context.Context, dialogueID, completionID string,
+	finishReason backend.FinishReason,
+	msg backend.ChatCompletionMessage,
+) {
+	log.WithFields(log.Fields{
+		"reason":            finishReason,
+		"dialogueID":        dialogueID,
+		"completionID":      completionID,
+		logging.KeyCallType: "logCompletion",
+		logging.KeyFile:     "main.go",
+	}).Debugf("%+v", msg)
 }
--- cmd/extension_ai/main.go
+++ cmd/extension_ai/main.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"net/http"
 	_ "net/http/pprof"
 
-	"github.com/ernestrc/blue/logging"
 	log "github.com/sirupsen/logrus"
-	configapi "unstable.build/go-tui/api/config"
-	"unstable.build/go-tui/cmd/extension_ai/backend"
-	"unstable.build/go-tui/cmd/extension_ai/backend/openai"
-	"unstable.build/go-tui/cmd/extension_ai/dialogue"
 	"unstable.build/go-tui/cmd/extension_ai/extension"
 	"unstable.build/go-tui/extension/process"
-)
-
-const (
-	defaultDefaultModel = openai.GPT3Dot5Turbo
-	defaultBaseURL      = "" // uses openai's default base URL
 )
 
 func main() {
@@ -27,86 +14,6 @@ func main() {
 		log.Println(http.ListenAndServe("localhost:8886", nil))
 	}()
 
-	openaiSvc := func(
-		config configapi.Config, availableModels map[string]int, model string,
-	) (backend.Service, error) {
-		apiKey, err := config.GetString("api_key")
-		if err != nil {
-			err = fmt.Errorf("failed to get 'api_key' from config: %w", err)
-			return nil, err
-		}
-		openaiConfig := openai.Config{
-			Model: model,
-		}
-		openaiConfig.BaseURL, err = config.GetString("base_url")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'base_url' from config: %w", err)
-				return nil, err
-			}
-			openaiConfig.BaseURL = defaultBaseURL
-		}
-		openaiConfig.FrequencyPenalty, err = config.GetFloat("frequency_penalty")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'frequency_penalty' from config: %w", err)
-				return nil, err
-			}
-		}
-		openaiConfig.MaxTokens, err = config.GetInt("max_tokens")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'max_tokens' from config: %w", err)
-				return nil, err
-			}
-		}
-		openaiConfig.PresencePenalty, err = config.GetFloat("presence_penalty")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'presence_penalty' from config: %w", err)
-				return nil, err
-			}
-		}
-		openaiConfig.Temperature, err = config.GetFloat("temperature")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'temperature' from config: %w", err)
-				return nil, err
-			}
-		}
-		openaiConfig.TopP, err = config.GetFloat("top_p")
-		if err != nil {
-			if !errors.Is(err, configapi.ErrNotFound) {
-				err = fmt.Errorf("failed to get 'top_p' from config: %w", err)
-				return nil, err
-			}
-		}
-		return openai.NewClient(apiKey, openaiConfig, availableModels), nil
-	}
-	grantee, perms := extension.GranteeWithService(openaiSvc,
-		openai.AvailableModels(), defaultDefaultModel,
-		dialogue.WithCompleter(dialogue.FuncCompleter(logCompletion)),
-		dialogue.WithInitialContext([]backend.ChatCompletionMessage{
-			{
-				Role: backend.RoleSystem,
-				Content: "You are a helpful coding assistant. Any code changes must be " +
-					" answered in diff format.",
-			},
-		}),
-	)
+	grantee, perms := extension.DefaultOpenAIGrantee()
 	process.Serve(grantee, perms...)
-}
-
-func logCompletion(
-	ctx context.Context, dialogueID, completionID string,
-	finishReason backend.FinishReason,
-	msg backend.ChatCompletionMessage,
-) {
-	log.WithFields(log.Fields{
-		"reason":            finishReason,
-		"dialogueID":        dialogueID,
-		"completionID":      completionID,
-		logging.KeyCallType: "logCompletion",
-		logging.KeyFile:     "main.go",
-	}).Debugf("%+v", msg)
 }
