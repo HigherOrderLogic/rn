cff4868bd906b929cf5da83b52545aa4d1e97f8d
ernestrc <ernest@unstable.build>
2023-12-11 21:35:48 -0800

    extension_ai: Add gpt-4 turbo

--- cmd/extension_ai/backend/openai/models.go
+++ cmd/extension_ai/backend/openai/models.go
@@ -7,6 +7,9 @@ const (
 	GPT3Dot5Turbo = openai.GPT3Dot5Turbo1106
 	// GPT4 is openai's gpt-4 model.
 	GPT4 = openai.GPT4
+
+	// GPT4Turbo is openai's gpt-4 turbo preview model.
+	GPT4Turbo = openai.GPT4TurboPreview
 )
 
 // AvailableModels returns a set with the available models and their
@@ -15,5 +18,6 @@ func AvailableModels() (ret map[string]int) {
 	return map[string]int{
 		GPT4:          8192,
 		GPT3Dot5Turbo: 16385,
+		GPT4Turbo:     8192,
 	}
 }
