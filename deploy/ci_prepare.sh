897eb57fa536ae4797b48fd11a4b30cf6a6ca49d
ernestrc <ernest@unstable.build>
2026-06-22 12:55:20 +0200

    Fix SSH key not being passed correctly in linux build container

--- deploy/ci_prepare.sh
+++ deploy/ci_prepare.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 
 mkdir -p ~/.ssh
-echo "$GIT_SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
 chmod 700 ~/.ssh
+printf '%s\n' "$GIT_SSH_KEY" | tr -d '\r' > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -t rsa git.unstable.build >> ~/.ssh/known_hosts
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
